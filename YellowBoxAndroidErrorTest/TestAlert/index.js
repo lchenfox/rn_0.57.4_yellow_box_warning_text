ac0634e581b42b5c2d3e33ab3ba563b92ac3cd4b
langke <lchenfox@foxmail.com>
2020-05-27 13:56:45 +0800

    Update: add lib directory

--- YellowBoxAndroidErrorTest/TestAlert/index.js
+++ YellowBoxAndroidErrorTest/TestAlert/index.js
@@ -8,21 +8,21 @@ import {
     ModalTitleProps,
     ModalContentProps,
     BackdropProps,
-} from './modal/type';
-import Modal from './modal/Modal';
-import DraggableView from './modal/components/DraggableView';
-import BaseModal from './modal/components/BaseModal';
-import BottomModal from './modal/components/BottomModal';
-import Backdrop from './modal/components/Backdrop';
-import ModalTitle from './modal/components/ModalTitle';
-import ModalFooter from './modal/components/ModalFooter';
-import ModalButton from './modal/components/ModalButton';
-import ModalContent from './modal/components/ModalContent';
-import Animation from './modal/animations/Animation';
-import FadeAnimation from './modal/animations/FadeAnimation';
-import ScaleAnimation from './modal/animations/ScaleAnimation';
-import SlideAnimation from './modal/animations/SlideAnimation';
-import WKAlert from "./alert/WKAlert";
+} from './lib/modal/type';
+import Modal from './lib/modal/Modal';
+import DraggableView from './lib/modal/components/DraggableView';
+import BaseModal from './lib/modal/components/BaseModal';
+import BottomModal from './lib/modal/components/BottomModal';
+import Backdrop from './lib/modal/components/Backdrop';
+import ModalTitle from './lib/modal/components/ModalTitle';
+import ModalFooter from './lib/modal/components/ModalFooter';
+import ModalButton from './lib/modal/components/ModalButton';
+import ModalContent from './lib/modal/components/ModalContent';
+import Animation from './lib/modal/animations/Animation';
+import FadeAnimation from './lib/modal/animations/FadeAnimation';
+import ScaleAnimation from './lib/modal/animations/ScaleAnimation';
+import SlideAnimation from './lib/modal/animations/SlideAnimation';
+import WKAlert from "./lib/alert/WKAlert";
 
 Modal.BottomModal = BottomModal;
 
